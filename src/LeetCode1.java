c38af3324213fc6470894af38e46ba6cc203a473
Saurabh Singh <saurabh030703@gmail.com>
2025-01-13 15:59:37 +0530

    solution add in leetcode1

--- src/LeetCode1.java
+++ src/LeetCode1.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class LeetCode1 {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        int[] x =new int[2];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    x[0]=i;
+                    x[1]=j;
+
+                }
+            }
+
+        }
+        return x;
+
+    }
 }
